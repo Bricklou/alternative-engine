55ae40f37cabf5097e3b573a2dfd207bd65f0aad
Louis Bailleau <louis.bailleau@free.fr>
2022-04-18 17:55:05 +0200

    Project base structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# alternative-engine
+
+This repos is an attempt to make a custom game engine from scratch using SDL2 and Vulkan.
---
+++ cmake/FindImGui.cmake
@@ -0,0 +1,21 @@
+add_library(imgui STATIC)
+
+set(IMGUI_DIR ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
+
+target_sources(imgui
+    PRIVATE
+        ${IMGUI_DIR}/imgui_demo.cpp
+        ${IMGUI_DIR}/imgui_draw.cpp
+        ${IMGUI_DIR}/imgui_tables.cpp
+        ${IMGUI_DIR}/imgui_widgets.cpp
+        ${IMGUI_DIR}/imgui.cpp
+
+    PRIVATE
+        ${IMGUI_DIR}/backends/imgui_impl_opengl3.cpp
+        ${IMGUI_DIR}/backends/imgui_impl_sdl.cpp
+)
+
+target_include_directories(imgui
+    PUBLIC ${IMGUI_DIR}
+    PUBLIC ${IMGUI_DIR}/backends
+)
